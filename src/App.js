51e5e37f6a1c37a55de5f0efbc0e3637fe5668e6
RAHUL JAIN <rahul.jain1@mphasis.com>
2022-05-13 16:47:52 +0530

    created-calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import mock from "./mock";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [input, setInput] = useState("");
+  const [prev, setPrev] = useState("0");
+  const [numb, setNumb] = useState(0);
+  const [symb, setSymb] = useState("");
+
+  function handler(e) {
+    const num = Number(e.target.value);
+    if (num <= 9 || e.target.value === ".") {
+      // let updateNum = Number(input.concat(e.target.value));
+      // setNumb(Number(input));
+      // console.log(numb, typeof(numb));
+      setInput(input.concat(e.target.value));
+    }
+    if (e.target.value === "RESET") {
+      setInput("");
+    }
+    if (e.target.value === "DEL") {
+      const dlt = input.split("");
+      dlt.pop();
+      // console.log(dlt.join())
+      // console.log(input)
+      setInput(dlt.join(""));
+    }
+    if (
+      e.target.value === "+" ||
+      e.target.value === "-" ||
+      e.target.value === "/" ||
+      e.target.value === "x"
+    ) {
+      setPrev(input);
+      setInput("");
+      setSymb(e.target.value);
+    }
+
+    if (e.target.value === "=") {
+      switch (symb) {
+        case "+":
+          let sum = Number(prev) + Number(input);
+          setInput(sum);
+          setSymb("");
+          break;
+        case "-":
+          let sub = Number(prev) - Number(input);
+          setInput(sub);
+          setSymb("");
+          break;
+        case "/":
+          let div = Number(prev) / Number(input);
+          setInput(div);
+          setSymb("");
+          break;
+        case "x":
+          let mul = Number(prev) * Number(input);
+          setInput(mul);
+          setSymb("");
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  let items = mock.keys.map((e) => (
+    <div className={e.class} key={e.id}>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={handler}
+        value={e.label}
+      >
+        {e.label}
+      </button>
     </div>
+  ));
+  return (
+    <>
+      <div className="container mt-5" style={{ width: "220px" }}>
+        <input
+          className="form-control"
+          type="text"
+          placeholder=""
+          aria-label="Disabled input example"
+          disabled
+          value={input}
+        />
+        <div className="row">{items}</div>
+      </div>
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/mock.js
@@ -0,0 +1,115 @@
+export default {
+    "keys": [{
+        "id":"7",
+        "value":"7",
+        "label": "7",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"8",
+        "value":"8",
+        "label": "8",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"9",
+        "value":"9",
+        "label": "9",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"10",
+        "value":"del",
+        "label": "DEL",
+        "class": "col-md-3  p-1"
+    },
+    {
+        "id":"4",
+        "value":"4",
+        "label": "4",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"5",
+        "value":"5",
+        "label": "5",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"6",
+        "value":"6",
+        "label": "6",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"11",
+        "value":"plus",
+        "label": "+",
+        "class": "col-md-3 p-1"
+    },
+    
+    {
+        "id":"1",
+        "value":"1",
+        "label": "1",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"2",
+        "value":"2",
+        "label": "2",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"3",
+        "value":"3",
+        "label": "3",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"12",
+        "value":"minus",
+        "label": "-",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"15",
+        "value":"dot",
+        "label": ".",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"0",
+        "value":"0",
+        "label": "0",
+        "class": "col-md-3 p-1"
+    },
+    {
+        "id":"14",
+        "value":"divide",
+        "label": "/",
+        "class": "col-md-3 p-1"
+    },   
+    {
+        "id":"13",
+        "value":"multiply",
+        "label": "x",
+        "class": "col-md-3 p-1"
+    },
+    
+    
+    {
+        "id":"16",
+        "value":"reset",
+        "label": "RESET",
+        "class": "col-md-6 mx-auto p-1"
+    },
+    {
+        "id":"17",
+        "value":"=",
+        "label": "=",
+        "class": "col-md-6 mx-auto p-1"
+    }
+
+]
+}
